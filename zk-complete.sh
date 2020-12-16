c9026d9b6576d3cd87aac0eead5e16593dbfd186
Brennan Barker <BrennanBarker@users.noreply.github.com>
2020-12-16 14:19:42 -0500

    add completion (broken?)

---
+++ zk-complete.sh
@@ -0,0 +1,32 @@
+#compdef zk
+
+_zk_completion() {
+    local -a completions
+    local -a completions_with_descriptions
+    local -a response
+    (( ! $+commands[zk] )) && return 1
+
+    response=("${(@f)$( env COMP_WORDS="${words[*]}" \
+                        COMP_CWORD=$((CURRENT-1)) \
+                        _ZK_COMPLETE="complete_zsh" \
+                        zk )}")
+
+    for key descr in ${(kv)response}; do
+      if [[ "$descr" == "_" ]]; then
+          completions+=("$key")
+      else
+          completions_with_descriptions+=("$key":"$descr")
+      fi
+    done
+
+    if [ -n "$completions_with_descriptions" ]; then
+        _describe -V unsorted completions_with_descriptions -U
+    fi
+
+    if [ -n "$completions" ]; then
+        compadd -U -V unsorted -a completions
+    fi
+    compstate[insert]="automenu"
+}
+
+compdef _zk_completion zk;
