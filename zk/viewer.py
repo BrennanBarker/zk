181f19e35d75be5d494fe02cce517dc3d194db97
Brennan Barker <BrennanBarker@users.noreply.github.com>
2021-03-21 20:15:38 -0400

    blueprints

--- zk/viewer.py
+++ zk/viewer.py
@@ -1,9 +1,30 @@
 import os
 from markdown import markdown
-from flask import Flask, render_template
+from flask import Flask, Blueprint, render_template
 
 from zk.utils import (get_note_html, get_title, get_note_path, get_id, 
                       all_notes, build_tag_index)
+
+
+notes_bp = Blueprint('note', __name__)
+
+@notes_bp.route('/')
+def notes_index(ctx):
+    return render_template('all.html', 
+                           notes=all_notes(ctx),
+                           get_id=get_id,
+                           get_title=get_title,
+                           page_title='All notes',
+                          )
+
+@notes_bp.route('/<note_id>')
+def view_note(ctx, note_id):
+    if note_id.endswith('.md'): note_id = note_id[:-3]
+    return render_template('note.html', 
+                           note_html=get_note_html(ctx, note_id), 
+                           page_title=get_title(get_note_path(ctx, note_id))
+                          )
+
 
 def create_app(ctx, test_config=None):
     # create and configure the app
@@ -17,21 +38,16 @@ def create_app(ctx, test_config=None):
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    @app.route('/all')
-    def all_notes_page():
-        return render_template('all.html', 
-                               notes=all_notes(ctx),
-                               get_id=get_id,
-                               get_title=get_title,
-                               page_title='All notes'
-                              )
+    app.register_blueprint(notes_bp)
+    
+    app.add_url_rule('/', endpoint='notes_index')
+    
     
     @app.route('/search')
     def search():
         return 'This will show the search page'
     
     @app.route('/tag/<tag_name>')
-    """Stopped here, make this a blueprint"""
     def tag(tag_name):
         if tag_name == 'all-tags':
             tag_index = build_tag_index(ctx)
@@ -46,11 +62,5 @@ def create_app(ctx, test_config=None):
     def graph():
         return 'This will show the graph'
     
-    @app.route('/note/<note_id>')
-    def view_note(note_id):
-        if note_id.endswith('.md'): note_id = note_id[:-3]
-        return render_template('note.html', 
-                               note_html=get_note_html(ctx, note_id), 
-                               page_title=get_title(get_note_path(ctx, note_id)))
-
+    
     return app
